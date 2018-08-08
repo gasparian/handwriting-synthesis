0e1e8cfc6a8a0cb1522b48c4d905873692f7e50a
Andrey Gasparyan <gasparian.andrey@yandex.ru>
2018-08-08 19:46:30 +0500

    parallel processing

--- create_strokes.py
+++ create_strokes.py
@@ -1,6 +1,6 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-#os.environ['CUDA_VISIBLE_DEVICES'] = '-1' #for CPU only predictions
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' #for CPU only predictions
 
 import pickle
 import logging
